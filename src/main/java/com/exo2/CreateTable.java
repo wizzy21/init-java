5c52e2ba73c797e37a3a1d67c6aeb1ea1b4328fd
wizzy21 <gigiwiz41@gmail.com>
2022-05-13 17:13:21 +0200

    correction exo2

--- src/main/java/com/exo2/CreateTable.java
+++ src/main/java/com/exo2/CreateTable.java
@@ -11,17 +11,22 @@ public class CreateTable {
 
         Scanner scanner = new Scanner(System.in);
         Integer number = null;
+        String operation = "+";
 
         while (number == null) {
             try {
                 System.out.print("Enter a number : ");
                 number = scanner.nextInt(); // Exception potentielly high, so the variable will stay  null
+<<<<<<< Updated upstream
                 scanner.close();
+=======
+                String op = scanner.nextLine();
+>>>>>>> Stashed changes
             } catch (InputMismatchException e) {
                 System.out.println("Attention ! You have to enter a number");
                 scanner.nextLine();
             } catch (Exception e) {
-                System.out.println("Oops an error has occured :(");
+                System.out.println("Oops an error has occured :");
                 return;
             }
         }
@@ -32,3 +37,27 @@ public class CreateTable {
 }
 
 }
+
+/*
+       Scanner scanner = new Scanner(System.in);
+        Integer number = null;
+
+        while (number == null) {
+            try {
+                System.out.print("Entrez un nombre : ");
+                number = scanner.nextInt(); // Exception potentiellement levée, donc la variable number reste à null
+            } catch (InputMismatchException e) {
+                System.out.println("Attention ! Vous devez entrez un nombre");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Mince une erreur s'est produite :(");
+                return;
+            }
+        }
+
+        scanner.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(String.format("%d x %d = %d", number, i, number * i));
+        }
+ */
