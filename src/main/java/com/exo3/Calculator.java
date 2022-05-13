48389a3a4327734a53327843b3e6fcab2e38aa7e
wizzy21 <gigiwiz41@gmail.com>
2022-05-13 08:47:29 +0200

    excerise 3 corrected

--- src/main/java/com/exo3/Calculator.java
+++ src/main/java/com/exo3/Calculator.java
@@ -1,52 +1,137 @@
 package com.exo3;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Calculator {
 
-    public static int add(int num1, int num2) Throws ArithmeticException{
-        int sum = 0;
-        try{
-            sum = num1 +num2;
-        } catch(ArithmeticException ae)
-        {
-            throw new ArithmeticException();
+    private final List<String> handledOperators = new ArrayList<String>() {{
+        add("+");
+        add("-");
+        add("*");
+        add("/");
+    }};
+
+    public void start() throws Exception {
+
+        Long result = retrieveRequestedOperation();
+
+        if (result == null) {
+            throw new Exception("Une erreur est survenue !");
         }
-        return sum;
 
+        System.out.println(String.format("Résultat : %d", result));
     }
 
-    public static int subtract(int num1, int num2) Throws ArithmeticException{
-        int sumSubtract = 0;
-        try{
-        sumSubtract = num1 *num2;
+   private Long retrieveRequestedOperation() {
+        Scanner scanner = new Scanner(System.in);
 
-    } catch(ArithmeticException ae){
-        throw new ArithmeticException();
-    }
-        return sumSubtract ;
+       Long firstNumber = null;
+      Long secondNumber = null;
+        String operator = null;
 
-    }
+        // On boucle tant que l'utilisateur ne rentre pas de valeur valide
+        while (firstNumber == null || secondNumber == null || operator == null) { // Tant que firstNumber, secondNumber et operator sont vides on reste à l'interieur de la boucle
+            try {
+                System.out.print("Saisir le premier nombre : ");
+                firstNumber = scanner.nextLong();
 
-    public static int multiplication(int num1, int num2) Throws ArithmeticException{
-        int sumMultiply =0;
-        try {
-             sumMultiply = num1 * num2;
+                System.out.print("Saisir le deuxieme nombre : ");
+                secondNumber = scanner.nextLong();
 
-        } catch(ArithmeticException ae){
-            throw new ArithmeticException();
+                System.out.print("Saisir l'opérateur arithmetique (+, -, *, /) : ");
+                operator = scanner.next();
+
+                //// From here the operator is not null it has something in it
+                /* We are looking to si if the operator entered by the user is in the list of operators that we know to handle
+                We inverse with ! the return valeur of the method contain to enter in our error handling in case the operator is not in the list
+                * */
+
+                if (!handledOperators.contains(operator)) {
+                    System.out.println(String.format("Votre opérateur [%s] n'est pas valide !\n", operator));
+                    operator = null;  // We force the variable to be null to be able to co,tinue to iterate in our lopp
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("Désolé votre entré n'est pas valide\n");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Une erreur est survenue lorsqu'on récupère les entrés de l'utilisateur\n");
+                return null;
             }
-        return sumMultiply;
-
-    }
-
-    public static int division(int num1, int num2)Throws ArithmeticException{
-int sumDivide =0;
-        try {
-        sumDivide = num1 / num2;
-        } catch(ArithmeticException ae){
-            throw new ArithmeticException();
         }
-        return sumDivide;
+
+        scanner.close();
+
+        return calculate(firstNumber, secondNumber, operator);
     }
 
+    protected Long calculate(Long firstNumber, Long secondNumber, String operator) {
 
+        Long result;
+
+        switch (operator) {
+
+            case "+":
+                result = add(firstNumber, secondNumber);
+                break;
+
+            case "-":
+                result = subtract(firstNumber, secondNumber);
+                break;
+
+            case "*":
+                result = multiply(firstNumber, secondNumber);
+                break;
+
+            case "/":
+                result = divide(firstNumber, secondNumber);
+                break;
+
+            default:
+                result = null;
+                break;
+        }
+
+        return result;
+    }
+
+    public long add(long a, long b) {
+        return a + b;
+    }
+
+    public long subtract(long a, long b) {
+        return a - b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public long divide(long a, long b) throws ArithmeticException {
+
+        if (b == 0) {
+            throw new ArithmeticException("Il est impossible d'effectuer une division par 0 !");
+        }
+
+        return a / b;
+    }
 }
+    /*public long modulo(int num1, int num2)throws ArithmeticException{
+        int myMoudlo;
+      myMoudlo = num1 / num2;
+
+        if (num2 == 0){
+            throw new ArithmeticException("Not possible to divide by zero!");
+        }
+        return myMoudlo;
+    }
+*/
+
+
+
+
+
+
+
--- src/main/java/com/exo3/Main.java
+++ src/main/java/com/exo3/Main.java
@@ -1,69 +1,16 @@
 package com.exo3;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
 
-        Integer num1 = null;
-        Integer num2 = null;
-        String operation = "+";
-        int result;
 
-        Scanner scan = new Scanner(System.in);
-        while ((num1 = null || num2 == null)) {
-            try {
-
-                //Addition
-                System.out.println("PLease enter a number");
-                number = scan.nextInt();
-                operation = scan.nextLine();
-            } catch (InputMismatchException imp) {
-                System.out.println("Attention, you need to put in a number only  ");
-            }
-            System.out.println("Please enter another number");
-            number = scan.nextInt();
-        }  catch(ImputMismatchException imp){
-            System.out.println("Oops an error has occured :");
+        try {
+            myCalculator.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
-   / System.out.println("Choisissez l'opération, appuyez sur : \n 1 + l'addition \n 2 + for substraction\n 3 for multiplicatiion \n 4 pour la division");
-    choice = scanner.nextInt();
-
-
-  if (choice == 1) {
-        calculator.addition(arg1,arg2);
-    }
-        else {
-        System.out.println("Recommencez ! ");
-    }
-
-
-
-switch(operation){
-    case"+":
-        result = Calculator.add(3,4);
-        System.out.println(String.format("result: %d * %d = %d", num1,num2, result);
-
-       //System.out.println("Result : %d + %d =%d ,
-       // System.out.println(num1 + " + " + num2 + " = " + result);
-
-        break;
-        case "-":
-        System.out.println(Calculator.subtract());
-        break;
-        case "*":
-        System.out.println(Calculator.multiplication());
-        break;
-        case "/":
-        System.out.println(Calculator.division());
-        break;
-        default:
-            System.out.println("Did not enter an operator (+,-,*,/");
-
-
-        }
-  scan.close();
-        }
-
+}
